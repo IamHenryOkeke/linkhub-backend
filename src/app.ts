424370a984ea4ab23778fed37efeb9a96ffd8d0f
Okeke Nmesomachukwu Henry <Iamhenryokeke@gmail.com>
2025-05-17 06:20:55 +0100

    feat: added index router for app

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,38 @@
+import express, { Request, Response, NextFunction } from 'express';
 import dotenv from 'dotenv';
-import express from 'express';
+import router from './routes/index.routes';
+import { CustomError } from './lib/type';
 
 dotenv.config();
-
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (_req, res) => {
-  res.send('Hello from Express + TypeScript!');
+// Use routes
+app.use('/', router);
+
+// 404 handler
+app.use((req, res) => {
+  res.status(404).json({
+    status: 404,
+    message: 'The route you are looking for does not exist.',
+  });
+});
+
+// Error handler
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err: CustomError, req: Request, res: Response, next: NextFunction) => {
+  const statusCode = err.statusCode || 500;
+  res.status(statusCode).json({
+    statusCode,
+    message: err.message || 'Something went wrong',
+    details: err.details ?? undefined,
+  });
 });
 
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}`);
 });
